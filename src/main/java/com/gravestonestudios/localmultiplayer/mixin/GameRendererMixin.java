8eb160a568e1140a51bfcf06e6dc26255b6f85ad
gravestonestudios00-byte <gravestonestudios00@gmail.com>
2026-05-30 22:37:00 -0700

    Use raw OpenGL shader for Player2 window blit

--- src/main/java/com/gravestonestudios/localmultiplayer/mixin/GameRendererMixin.java
+++ src/main/java/com/gravestonestudios/localmultiplayer/mixin/GameRendererMixin.java
@@ -5,16 +5,14 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GLCapabilities;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -46,6 +44,8 @@ public abstract class GameRendererMixin {
     @Shadow
     public abstract void renderWorld(float tickDelta, long limitTime, MatrixStack matrices);
 
+    private static int p2BlitProgram = 0;
+
     @Inject(method = "render", at = @At("TAIL"))
     private void localmultiplayer$renderSecondPlayerView(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
         if (client.world == null || client.player == null) return;
@@ -62,8 +62,6 @@ public abstract class GameRendererMixin {
         Framebuffer p2Framebuffer = LocalMultiplayerClient.getSecondPlayerFramebuffer();
         long mainWindow = client.getWindow().getHandle();
 
-        // Stability rule: do not touch the second OpenGL context until Player2 and the framebuffer exist.
-        // The previous version switched contexts even for the waiting screen, which can hard-close Minecraft on join.
         if (playerTwo == null || p2Framebuffer == null || client.world == null) {
             return;
         }
@@ -125,38 +123,7 @@ public abstract class GameRendererMixin {
                 int[] p2h = new int[1];
                 GLFW.glfwGetFramebufferSize(p2Window, p2w, p2h);
 
-                GL11.glViewport(0, 0, p2w[0], p2h[0]);
-                GL11.glDisable(GL11.GL_DEPTH_TEST);
-                GL11.glClearColor(0f, 0f, 0f, 1f);
-                GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-
-                RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-                RenderSystem.setShaderTexture(0, p2Framebuffer.getColorAttachment());
-                RenderSystem.disableDepthTest();
-
-                GL11.glMatrixMode(GL11.GL_PROJECTION);
-                GL11.glPushMatrix();
-                GL11.glLoadIdentity();
-                GL11.glOrtho(0, 1, 0, 1, -1, 1);
-
-                GL11.glMatrixMode(GL11.GL_MODELVIEW);
-                GL11.glPushMatrix();
-                GL11.glLoadIdentity();
-
-                Tessellator tessellator = Tessellator.getInstance();
-                BufferBuilder bufferBuilder = tessellator.getBuffer();
-                bufferBuilder.begin(net.minecraft.client.render.VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-                bufferBuilder.vertex(0.0, 1.0, 0.0).texture(0.0f, 0.0f).next();
-                bufferBuilder.vertex(1.0, 1.0, 0.0).texture(1.0f, 0.0f).next();
-                bufferBuilder.vertex(1.0, 0.0, 0.0).texture(1.0f, 1.0f).next();
-                bufferBuilder.vertex(0.0, 0.0, 0.0).texture(0.0f, 1.0f).next();
-                tessellator.draw();
-
-                GL11.glPopMatrix();
-                GL11.glMatrixMode(GL11.GL_PROJECTION);
-                GL11.glPopMatrix();
-                GL11.glMatrixMode(GL11.GL_MODELVIEW);
-
+                drawTextureToSecondWindow(p2Framebuffer.getColorAttachment(), p2w[0], p2h[0]);
                 GLFW.glfwSwapBuffers(p2Window);
             } finally {
                 GLFW.glfwMakeContextCurrent(previousContext);
@@ -174,6 +141,82 @@ public abstract class GameRendererMixin {
             RenderSystem.viewport(0, 0, client.getWindow().getFramebufferWidth(), client.getWindow().getFramebufferHeight());
             LocalMultiplayerClient.isRenderingSecondView = false;
         }
+    }
+
+    private static void drawTextureToSecondWindow(int textureId, int width, int height) {
+        GL11.glViewport(0, 0, Math.max(1, width), Math.max(1, height));
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+        GL11.glClearColor(0f, 0f, 0f, 1f);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+        if (p2BlitProgram == 0) {
+            p2BlitProgram = createP2BlitProgram();
+        }
+
+        GL20.glUseProgram(p2BlitProgram);
+        GL11.glActiveTexture(GL11.GL_TEXTURE0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId);
+        int samplerLocation = GL20.glGetUniformLocation(p2BlitProgram, "uTexture");
+        GL20.glUniform1i(samplerLocation, 0);
+
+        GL11.glBegin(GL11.GL_TRIANGLES);
+        GL11.glTexCoord2f(0f, 0f); GL11.glVertex2f(-1f, 1f);
+        GL11.glTexCoord2f(1f, 0f); GL11.glVertex2f(1f, 1f);
+        GL11.glTexCoord2f(1f, 1f); GL11.glVertex2f(1f, -1f);
+
+        GL11.glTexCoord2f(0f, 0f); GL11.glVertex2f(-1f, 1f);
+        GL11.glTexCoord2f(1f, 1f); GL11.glVertex2f(1f, -1f);
+        GL11.glTexCoord2f(0f, 1f); GL11.glVertex2f(-1f, -1f);
+        GL11.glEnd();
+
+        GL20.glUseProgram(0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+    }
+
+    private static int createP2BlitProgram() {
+        String vertexSource = "#version 120\n" +
+                "varying vec2 vTex;\n" +
+                "void main() {\n" +
+                "    gl_Position = gl_Vertex;\n" +
+                "    vTex = gl_MultiTexCoord0.xy;\n" +
+                "}\n";
+
+        String fragmentSource = "#version 120\n" +
+                "uniform sampler2D uTexture;\n" +
+                "varying vec2 vTex;\n" +
+                "void main() {\n" +
+                "    gl_FragColor = texture2D(uTexture, vTex);\n" +
+                "}\n";
+
+        int vertexShader = compileShader(GL20.GL_VERTEX_SHADER, vertexSource);
+        int fragmentShader = compileShader(GL20.GL_FRAGMENT_SHADER, fragmentSource);
+        int program = GL20.glCreateProgram();
+        GL20.glAttachShader(program, vertexShader);
+        GL20.glAttachShader(program, fragmentShader);
+        GL20.glLinkProgram(program);
+
+        if (GL20.glGetProgrami(program, GL20.GL_LINK_STATUS) == GL11.GL_FALSE) {
+            String log = GL20.glGetProgramInfoLog(program);
+            LOGGER.error("[LocalMultiplayer] Failed to link P2 blit shader: {}", log);
+        }
+
+        GL20.glDeleteShader(vertexShader);
+        GL20.glDeleteShader(fragmentShader);
+        return program;
+    }
+
+    private static int compileShader(int type, String source) {
+        int shader = GL20.glCreateShader(type);
+        GL20.glShaderSource(shader, source);
+        GL20.glCompileShader(shader);
+
+        if (GL20.glGetShaderi(shader, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE) {
+            String log = GL20.glGetShaderInfoLog(shader);
+            LOGGER.error("[LocalMultiplayer] Failed to compile P2 shader: {}", log);
+        }
+
+        return shader;
     }
 
     @Redirect(
